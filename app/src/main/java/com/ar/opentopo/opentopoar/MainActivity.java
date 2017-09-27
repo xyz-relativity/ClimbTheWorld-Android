f285b15cab26441aea268e3d80fc3a7a0e27c13f
xyz32 <xyz.relativity@gmail.com>
2017-09-27 08:08:15 -0400

    Create view activity. Move strings to strings.xml

--- app/src/main/java/com/ar/opentopo/opentopoar/MainActivity.java
+++ app/src/main/java/com/ar/opentopo/opentopoar/MainActivity.java
@@ -1,5 +1,6 @@
 package com.ar.opentopo.opentopoar;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -15,5 +16,11 @@ public class MainActivity extends AppCompatActivity {
     public void onClickButtonExit(View v)
     {
         System.exit(0);
+    }
+
+    public void onClickButtonViewTopo(View v)
+    {
+        Intent intent = new Intent(MainActivity.this, ViewTopo.class);
+        startActivity(intent);
     }
 }
---
+++ app/src/main/java/com/ar/opentopo/opentopoar/ViewTopo.java
@@ -0,0 +1,13 @@
+package com.ar.opentopo.opentopoar;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ViewTopo extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_view_topo);
+    }
+}
