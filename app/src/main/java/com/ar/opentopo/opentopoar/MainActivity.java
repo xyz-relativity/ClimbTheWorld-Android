1d189048d756b1f7765f8dc5948401ef0bcdd0ef
xyz32 <xyz.relativity@gmail.com>
2017-12-09 10:15:03 -0500

    Add gyroscope test.

--- app/src/main/java/com/ar/opentopo/opentopoar/MainActivity.java
+++ app/src/main/java/com/ar/opentopo/opentopoar/MainActivity.java
@@ -1,6 +1,10 @@
 package com.ar.opentopo.opentopoar;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.hardware.Sensor;
+import android.hardware.SensorManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -13,6 +17,11 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        if (((SensorManager) getSystemService(SENSOR_SERVICE)).getSensorList(Sensor.TYPE_GYROSCOPE).size() == 0)
+        {
+            displayHardwareMissingWarning();
+        }
     }
 
     public void onClickButtonExit(View v)
@@ -24,5 +33,19 @@ public class MainActivity extends AppCompatActivity {
     {
         Intent intent = new Intent(MainActivity.this, ViewTopoActivity.class);
         startActivity(intent);
+    }
+
+    private void displayHardwareMissingWarning() {
+        AlertDialog ad = new AlertDialog.Builder(this).create();
+        ad.setCancelable(false); // This blocks the 'BACK' button
+        ad.setTitle(getResources().getString(R.string.gyroscope_missing));
+        ad.setMessage(getResources().getString(R.string.gyroscope_missing_message));
+        ad.setButton(DialogInterface.BUTTON_NEUTRAL, getResources().getString(R.string.OK), new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                dialog.dismiss();
+            }
+        });
+        ad.show();
     }
 }
