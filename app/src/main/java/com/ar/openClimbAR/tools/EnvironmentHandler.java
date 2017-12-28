7c46133499ee762df70692d56c59ea8efa0c963a
xyz32 <xyz.relativity@gmail.com>
2017-12-28 10:30:26 -0500

    Set taint for map markers.

--- app/src/main/java/com/ar/openClimbAR/tools/EnvironmentHandler.java
+++ app/src/main/java/com/ar/openClimbAR/tools/EnvironmentHandler.java
@@ -1,6 +1,8 @@
 package com.ar.openClimbAR.tools;
 
 import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.content.res.ColorStateList;
 import android.graphics.PorterDuff;
 import android.graphics.drawable.Drawable;
@@ -21,8 +23,11 @@ import org.osmdroid.tileprovider.tilesource.TileSourceFactory;
 import org.osmdroid.util.GeoPoint;
 import org.osmdroid.views.MapView;
 import org.osmdroid.views.overlay.FolderOverlay;
+import org.osmdroid.views.overlay.ItemizedIconOverlay;
+import org.osmdroid.views.overlay.ItemizedOverlayWithFocus;
 import org.osmdroid.views.overlay.Marker;
 import org.osmdroid.views.overlay.Overlay;
+import org.osmdroid.views.overlay.OverlayItem;
 import org.osmdroid.views.overlay.mylocation.MyLocationNewOverlay;
 
 import java.io.BufferedReader;
@@ -30,6 +35,7 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.nio.charset.Charset;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Locale;
 import java.util.Map;
@@ -284,12 +290,44 @@ public class EnvironmentHandler {
         osmMap.setMapOrientation(-observer.degAzimuth);
     }
 
-    private void addMapMarker(PointOfInterest poi) {
-        FolderOverlay myMarkersFolder = new FolderOverlay();
-        List<Overlay> list = myMarkersFolder.getItems();
+    private void addMapMarker(final PointOfInterest poi) {
+//        FolderOverlay myMarkersFolder = new FolderOverlay();
+//        List<Overlay> list = myMarkersFolder.getItems();
+//
+//        Drawable nodeIcon = activity.getResources().getDrawable(R.drawable.marker_default);
+//
+//        float remapGradeScale = ArUtils.remapScale(0f,
+//                GradeConverter.getConverter().maxGrades,
+//                0f,
+//                1f,
+//                poi.getLevel());
+//        nodeIcon.setTintList(ColorStateList.valueOf(android.graphics.Color.HSVToColor(new float[]{(float)remapGradeScale*120f,1f,1f})));
+//        nodeIcon.setTintMode(PorterDuff.Mode.MULTIPLY);
+//
+//        Marker nodeMarker = new Marker(osmMap);
+//        nodeMarker.setPosition(new GeoPoint(poi.decimalLatitude, poi.decimalLongitude));
+//        nodeMarker.setIcon(nodeIcon);
+//        nodeMarker.setTitle(poi.name);
+//        nodeMarker.setSubDescription(GradeConverter.getConverter().getGradeFromOrder("UIAA", poi.getLevel()) +" (UIAA)");
+//        nodeMarker.setImage(nodeIcon);
+//        nodeMarker.setOnMarkerClickListener(new Marker.OnMarkerClickListener() {
+//            @Override
+//            public boolean onMarkerClick(Marker marker, MapView mapView) {
+//                marker.showInfoWindow();
+//                return true;
+//            }
+//        });
+//
+//        //put into FolderOverlay list
+//        list.add(nodeMarker);
+//
+//        osmMap.getOverlays().add(myMarkersFolder);
+//        osmMap.invalidate();
 
+
+        ArrayList<OverlayItem> items = new ArrayList<>();
+        OverlayItem item = new OverlayItem(String.valueOf(poi.getLevel()), poi.name, new GeoPoint(poi.decimalLatitude, poi.decimalLongitude));
         Drawable nodeIcon = activity.getResources().getDrawable(R.drawable.route_icon_small);
-
         float remapGradeScale = ArUtils.remapScale(0f,
                 GradeConverter.getConverter().maxGrades,
                 0f,
@@ -297,37 +335,15 @@ public class EnvironmentHandler {
                 poi.getLevel());
         nodeIcon.setTintList(ColorStateList.valueOf(android.graphics.Color.HSVToColor(new float[]{(float)remapGradeScale*120f,1f,1f})));
         nodeIcon.setTintMode(PorterDuff.Mode.MULTIPLY);
+        item.setMarker(nodeIcon);
 
-
-        Marker nodeMarker = new Marker(osmMap);
-        nodeMarker.setPosition(new GeoPoint(poi.decimalLatitude, poi.decimalLongitude));
-        nodeMarker.setIcon(nodeIcon);
-        nodeMarker.setTitle(poi.name);
-        nodeMarker.setSubDescription(GradeConverter.getConverter().getGradeFromOrder("UIAA", poi.getLevel()) +" (UIAA)");
-        nodeMarker.setImage(nodeIcon);
-        nodeMarker.setOnMarkerClickListener(new Marker.OnMarkerClickListener() {
-            @Override
-            public boolean onMarkerClick(Marker marker, MapView mapView) {
-                marker.showInfoWindow();
-                return true;
-            }
-        });
-
-        //put into FolderOverlay list
-        list.add(nodeMarker);
-
-        osmMap.getOverlays().add(myMarkersFolder);
-        osmMap.invalidate();
-
-        /*
-        ArrayList<OverlayItem> items = new ArrayList<>();
-        items.add(new OverlayItem(poi.name, String.valueOf(poi.getLevel()), new GeoPoint(poi.decimalLatitude, poi.decimalLongitude)));
+        items.add(item);
 
         ItemizedOverlayWithFocus<OverlayItem> mOverlay = new ItemizedOverlayWithFocus<>(items,
                 new ItemizedIconOverlay.OnItemGestureListener<OverlayItem>() {
                     @Override
                     public boolean onItemSingleTapUp(final int index, final OverlayItem item) {
-                        return true;
+                        return false;
                     }
                     @Override
                     public boolean onItemLongPress(final int index, final OverlayItem item) {
@@ -337,7 +353,7 @@ public class EnvironmentHandler {
         mOverlay.setFocusItemsOnTap(false);
 
         osmMap.getOverlays().add(mOverlay);
-         */
+        osmMap.invalidate();
     }
 
 
