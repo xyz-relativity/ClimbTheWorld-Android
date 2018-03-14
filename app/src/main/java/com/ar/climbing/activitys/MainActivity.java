6eb542fffcc04c1c72f5abc3efccbac6de0a861d
xyz32 <xyz.relativity@gmail.com>
2018-03-14 08:55:20 -0400

    Initial supportMe activity.

--- app/src/main/java/com/ar/climbing/activitys/MainActivity.java
+++ app/src/main/java/com/ar/climbing/activitys/MainActivity.java
@@ -104,6 +104,8 @@ public class MainActivity extends AppCompatActivity {
                 break;
 
             case R.id.ButtonDonate:
+                intent = new Intent(MainActivity.this, SupportMeActivity.class);
+                startActivity(intent);
                 break;
         }
     }
---
+++ app/src/main/java/com/ar/climbing/activitys/SupportMeActivity.java
@@ -0,0 +1,15 @@
+package com.ar.climbing.activitys;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.ar.climbing.R;
+
+public class SupportMeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_support_me);
+    }
+}
