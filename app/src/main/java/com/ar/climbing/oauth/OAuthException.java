7d0b1be1bc1c887839c40abb6932f6d78caf42f2
xyz32 <xyz.relativity@gmail.com>
2018-03-29 10:31:43 -0400

    OAuth code cleanup.

---
+++ app/src/main/java/com/ar/climbing/oauth/OAuthException.java
@@ -0,0 +1,10 @@
+package com.ar.climbing.oauth;
+
+import java.io.IOException;
+
+public class OAuthException extends IOException {
+
+    public OAuthException(final String string) {
+        super(string);
+    }
+}
