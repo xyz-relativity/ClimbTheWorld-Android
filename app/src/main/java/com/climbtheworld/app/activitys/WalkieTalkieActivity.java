3712a7ad5d4a0b44990e48b2a20dd7b9f4cbf573
xyz32 <xyz.relativity@gmail.com>
2018-07-27 10:52:15 -0400

    Add menu to setup bluetooth and wifi.

--- app/src/main/java/com/climbtheworld/app/activitys/WalkieTalkieActivity.java
+++ app/src/main/java/com/climbtheworld/app/activitys/WalkieTalkieActivity.java
@@ -2,10 +2,16 @@ package com.climbtheworld.app.activitys;
 
 import android.content.ComponentName;
 import android.content.Intent;
+import android.graphics.Color;
 import android.provider.Settings;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.view.MotionEvent;
 import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.PopupMenu;
 
 import com.climbtheworld.app.R;
 
@@ -15,24 +21,75 @@ public class WalkieTalkieActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_walkie_talkie);
+
+        Button ptt = findViewById(R.id.pushToTalkButton);
+        ptt.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                ImageView mic = findViewById(R.id.microphoneIcon);
+                switch ( event.getAction() ) {
+                    case MotionEvent.ACTION_DOWN:
+                        mic.setColorFilter(Color.argb(200, 0, 255, 0),android.graphics.PorterDuff.Mode.MULTIPLY);
+                        break;
+                    case MotionEvent.ACTION_UP:
+                        mic.setColorFilter(Color.argb(200, 255, 255, 255),android.graphics.PorterDuff.Mode.MULTIPLY);
+                        break;
+                }
+                return true;
+            }
+        });
     }
 
     public void onClick(View v) {
+        //Creating the instance of PopupMenu
+        PopupMenu popup = new PopupMenu(WalkieTalkieActivity.this, v);
         switch (v.getId()) {
             case R.id.bluetoothMenu:
-                Intent intentOpenBluetoothSettings = new Intent();
-                intentOpenBluetoothSettings.setAction(android.provider.Settings.ACTION_BLUETOOTH_SETTINGS);
-                startActivity(intentOpenBluetoothSettings);
+                //Inflating the Popup using xml file
+                popup.getMenuInflater().inflate(R.menu.bluetooth_options, popup.getMenu());
+
+                //registering popup with OnMenuItemClickListener
+                popup.setOnMenuItemClickListener(new PopupMenu.OnMenuItemClickListener() {
+                    public boolean onMenuItemClick(MenuItem item) {
+                        switch (item.getItemId()) {
+                            case R.id.bluetoothSettings:
+                                Intent intentOpenBluetoothSettings = new Intent();
+                                intentOpenBluetoothSettings.setAction(android.provider.Settings.ACTION_BLUETOOTH_SETTINGS);
+                                startActivity(intentOpenBluetoothSettings);
+                                return true;
+                        }
+                        return false;
+                    }
+                });
+                popup.show();//showing popup menu
                 break;
 
             case R.id.wifiMenu:
-                final Intent intent = new Intent(Intent.ACTION_MAIN, null);
-                intent.addCategory(Intent.CATEGORY_LAUNCHER);
-                final ComponentName cn = new ComponentName("com.android.settings", "com.android.settings.TetherSettings");
-                intent.setComponent(cn);
-                intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
-                startActivity( intent);
-                break;
+                //Inflating the Popup using xml file
+                popup.getMenuInflater().inflate(R.menu.wifi_options, popup.getMenu());
+
+                //registering popup with OnMenuItemClickListener
+                popup.setOnMenuItemClickListener(new PopupMenu.OnMenuItemClickListener() {
+                    public boolean onMenuItemClick(MenuItem item) {
+                        switch (item.getItemId()) {
+                            case R.id.wifiSettings:
+                                Intent intentOpenBluetoothSettings = new Intent();
+                                intentOpenBluetoothSettings.setAction(Settings.ACTION_WIFI_SETTINGS);
+                                startActivity(intentOpenBluetoothSettings);
+                                return true;
+                            case R.id.hotspotWifiSettings:
+                                final Intent intent = new Intent(Intent.ACTION_MAIN, null);
+                                intent.addCategory(Intent.CATEGORY_LAUNCHER);
+                                final ComponentName cn = new ComponentName("com.android.settings", "com.android.settings.TetherSettings");
+                                intent.setComponent(cn);
+                                intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                                startActivity( intent);
+                                return true;
+                        }
+                        return false;
+                    }
+                });
+                popup.show();//showing popup menu
         }
     }
 }
