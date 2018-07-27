0af65f46c816797f86b808f2b3a4712801101ee0
xyz32 <xyz.relativity@gmail.com>
2018-07-27 09:38:57 -0400

    Implement the skeleton UI for the walkie-talkie.

--- app/src/main/java/com/climbtheworld/app/activitys/WalkieTalkieActivity.java
+++ app/src/main/java/com/climbtheworld/app/activitys/WalkieTalkieActivity.java
@@ -1,6 +1,8 @@
 package com.climbtheworld.app.activitys;
 
+import android.content.ComponentName;
 import android.content.Intent;
+import android.provider.Settings;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -16,8 +18,21 @@ public class WalkieTalkieActivity extends AppCompatActivity {
     }
 
     public void onClick(View v) {
-        Intent intentOpenBluetoothSettings = new Intent();
-        intentOpenBluetoothSettings.setAction(android.provider.Settings.ACTION_BLUETOOTH_SETTINGS);
-        startActivity(intentOpenBluetoothSettings);
+        switch (v.getId()) {
+            case R.id.bluetoothMenu:
+                Intent intentOpenBluetoothSettings = new Intent();
+                intentOpenBluetoothSettings.setAction(android.provider.Settings.ACTION_BLUETOOTH_SETTINGS);
+                startActivity(intentOpenBluetoothSettings);
+                break;
+
+            case R.id.wifiMenu:
+                final Intent intent = new Intent(Intent.ACTION_MAIN, null);
+                intent.addCategory(Intent.CATEGORY_LAUNCHER);
+                final ComponentName cn = new ComponentName("com.android.settings", "com.android.settings.TetherSettings");
+                intent.setComponent(cn);
+                intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                startActivity( intent);
+                break;
+        }
     }
 }
