84ca5573625b2006778407fe1bb7ce63955c7f74
xyz32 <xyz.relativity@gmail.com>
2019-02-08 15:43:50 -0500

    Use fragment tags for advanced editor.

--- app/src/main/java/com/climbtheworld/app/activities/EditNodeAdvancedActivity.java
+++ app/src/main/java/com/climbtheworld/app/activities/EditNodeAdvancedActivity.java
@@ -1,27 +1,21 @@
 package com.climbtheworld.app.activities;
 
-import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.EditText;
-import android.widget.LinearLayout;
-import android.widget.ScrollView;
 
 import com.climbtheworld.app.R;
+import com.climbtheworld.app.osm.editor.ITags;
+import com.climbtheworld.app.osm.editor.OtherTags;
 import com.climbtheworld.app.storage.database.GeoNode;
 
 import org.json.JSONException;
-import org.json.JSONObject;
-
-import java.util.Iterator;
 
 public class EditNodeAdvancedActivity extends AppCompatActivity implements View.OnClickListener {
-    LinearLayout scrollViewContainer;
     GeoNode poi;
+    ITags tags;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -38,74 +32,21 @@ public class EditNodeAdvancedActivity extends AppCompatActivity implements View.
             return;
         }
 
-        scrollViewContainer = findViewById(R.id.scrollViewContainer);
+        ViewGroup tagsView = findViewById(R.id.tagsView);
 
-        JSONObject tags = poi.getTags();
-        Iterator<String> keyIt = tags.keys();
-        while (keyIt.hasNext()) {
-            String key = keyIt.next();
-
-            LayoutInflater inflater = (LayoutInflater) this.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
-            View tagView = inflater.inflate(R.layout.list_item_json_entry, null);
-
-            ((EditText)tagView.findViewById(R.id.editTag)).setText(key);
-            ((EditText)tagView.findViewById(R.id.editValue)).setText(tags.optString(key));
-            tagView.findViewById(R.id.buttonDeleteField).setOnClickListener(this);
-
-            scrollViewContainer.addView(tagView, scrollViewContainer.getChildCount() - 1);
-        }
+        tags = new OtherTags(poi, this, tagsView);
     }
 
     @Override
     public void onClick(View view) {
         switch (view.getId()) {
-            case R.id.buttonDeleteField:
-                scrollViewContainer.removeView(((ViewGroup) view.getParent()));
-            break;
-
-            case R.id.buttonAddNew:
-                LayoutInflater inflater = (LayoutInflater) this.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
-                View tagView = inflater.inflate(R.layout.list_item_json_entry, null);
-
-                ((EditText)tagView.findViewById(R.id.editTag)).setText("");
-                ((EditText)tagView.findViewById(R.id.editValue)).setText("");
-                tagView.findViewById(R.id.buttonDeleteField).setOnClickListener(this);
-
-                scrollViewContainer.addView(tagView);
-
-                scrollViewContainer.post(new Runnable() {
-                    @Override
-                    public void run() {
-                        ((ScrollView)scrollViewContainer.getParent()).fullScroll(View.FOCUS_DOWN);
-                    }
-                });
-            break;
-
             case R.id.buttonCancel:
                 setResult(RESULT_CANCELED);
                 finish();
                 break;
 
             case R.id.buttonSave:
-                JSONObject newTags = new JSONObject();
-                for (int i = 0; i < scrollViewContainer.getChildCount(); i++) {
-                    View child = scrollViewContainer.getChildAt(i);
-
-                    if (!(child instanceof LinearLayout)) {
-                        continue;
-                    }
-
-                    try {
-                        String key = ((EditText)child.findViewById(R.id.editTag)).getText().toString();
-                        String value = ((EditText)child.findViewById(R.id.editValue)).getText().toString();
-                        if (!key.isEmpty()) {
-                            newTags.put(key, value);
-                        }
-                    } catch (JSONException e) {
-                        e.printStackTrace();
-                    }
-                }
-                poi.setTags(newTags);
+                tags.SaveToNode(poi);
 
                 Intent intent = new Intent();
                 intent.putExtra("nodeJson", poi.toJSONString());
