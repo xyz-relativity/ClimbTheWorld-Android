2981e5be0a40c0a1382d2e4570fde743112434a5
xyz32 <xyz.relativity@gmail.com>
2019-03-03 22:21:44 -0500

    Dynamically discover sampling rates.

--- app/src/main/java/com/climbtheworld/app/intercon/audiotools/IRecordingListener.java
+++ app/src/main/java/com/climbtheworld/app/intercon/audiotools/IRecordingListener.java
@@ -1,14 +1,30 @@
 package com.climbtheworld.app.intercon.audiotools;
 
 import android.media.AudioFormat;
-import android.media.AudioTrack;
+import android.media.AudioRecord;
 
 public interface IRecordingListener {
-    int AUDIO_SAMPLE_RATE = 8000;
+    class RecorderHelper {
+        static int AUDIO_SAMPLE_RATE = 8000;
+        static int AUDIO_BUFFER_SIZE = 0;
+
+        static {
+            for (int rate : new int[] {8000, 11025, 16000, 22050, 44100}) {  // add the rates you wish to check against
+                int bufferSize = AudioRecord.getMinBufferSize(rate, AUDIO_CHANNELS_IN, AUDIO_ENCODING);
+                if (bufferSize > 0) {
+                    AUDIO_SAMPLE_RATE = rate;
+                    AUDIO_BUFFER_SIZE = bufferSize;
+                    break;
+                }
+            }
+        }
+    }
+
+    int AUDIO_SAMPLE_RATE = RecorderHelper.AUDIO_SAMPLE_RATE;
     int AUDIO_CHANNELS_IN = AudioFormat.CHANNEL_IN_MONO;
     int AUDIO_CHANNELS_OUT = AudioFormat.CHANNEL_OUT_MONO;
     int AUDIO_ENCODING = AudioFormat.ENCODING_PCM_16BIT;
-    int AUDIO_BUFFER_SIZE = AudioTrack.getMinBufferSize(AUDIO_SAMPLE_RATE, AUDIO_CHANNELS_IN, AUDIO_ENCODING);
+    int AUDIO_BUFFER_SIZE = RecorderHelper.AUDIO_BUFFER_SIZE;
     void onRecordingStarted();
     void onRawAudio(byte[] frame, int numberOfReadBytes);
     void onAudio(byte[] frame, int numberOfReadBytes, double energy, double rms);
