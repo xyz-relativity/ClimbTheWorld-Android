fb2b84157cf8f5368320e0d2cb336cf5559a58ee
xyz32 <xyz.relativity@gmail.com>
2019-03-04 08:02:08 -0500

    Code styling.

--- app/src/main/java/com/climbtheworld/app/intercon/audiotools/IRecordingListener.java
+++ app/src/main/java/com/climbtheworld/app/intercon/audiotools/IRecordingListener.java
@@ -4,6 +4,16 @@ import android.media.AudioFormat;
 import android.media.AudioRecord;
 
 public interface IRecordingListener {
+    int AUDIO_SAMPLE_RATE = RecorderHelper.AUDIO_SAMPLE_RATE;
+    int AUDIO_CHANNELS_IN = AudioFormat.CHANNEL_IN_MONO;
+    int AUDIO_CHANNELS_OUT = AudioFormat.CHANNEL_OUT_MONO;
+    int AUDIO_ENCODING = AudioFormat.ENCODING_PCM_16BIT;
+    int AUDIO_BUFFER_SIZE = RecorderHelper.AUDIO_BUFFER_SIZE;
+    void onRecordingStarted();
+    void onRawAudio(byte[] frame, int numberOfReadBytes);
+    void onAudio(byte[] frame, int numberOfReadBytes, double energy, double rms);
+    void onRecordingDone();
+
     class RecorderHelper {
         static int AUDIO_SAMPLE_RATE = 8000;
         static int AUDIO_BUFFER_SIZE = 0;
@@ -19,14 +29,4 @@ public interface IRecordingListener {
             }
         }
     }
-
-    int AUDIO_SAMPLE_RATE = RecorderHelper.AUDIO_SAMPLE_RATE;
-    int AUDIO_CHANNELS_IN = AudioFormat.CHANNEL_IN_MONO;
-    int AUDIO_CHANNELS_OUT = AudioFormat.CHANNEL_OUT_MONO;
-    int AUDIO_ENCODING = AudioFormat.ENCODING_PCM_16BIT;
-    int AUDIO_BUFFER_SIZE = RecorderHelper.AUDIO_BUFFER_SIZE;
-    void onRecordingStarted();
-    void onRawAudio(byte[] frame, int numberOfReadBytes);
-    void onAudio(byte[] frame, int numberOfReadBytes, double energy, double rms);
-    void onRecordingDone();
 }
