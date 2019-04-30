43ccf1655b6d41b2f166e8400af32b3b90ea5376
xyz32 <xyz.relativity@gmail.com>
2019-04-30 08:09:06 -0400

    Add icons for all unit converters.

---
+++ app/src/main/java/com/climbtheworld/app/converter/LengthConverter.java
@@ -0,0 +1,142 @@
+package com.climbtheworld.app.converter;
+
+import android.content.Intent;
+import android.support.annotation.LayoutRes;
+import android.support.v7.app.AppCompatActivity;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.BaseAdapter;
+import android.widget.ListView;
+import android.widget.Spinner;
+import android.widget.TextView;
+
+import com.climbtheworld.app.R;
+import com.climbtheworld.app.activities.UnitConverterGradesAdvancedActivity;
+import com.climbtheworld.app.tools.GradeSystem;
+import com.climbtheworld.app.utils.Configs;
+import com.climbtheworld.app.utils.Globals;
+
+import java.util.List;
+
+public class LengthConverter extends ConverterFragment {
+
+    private Spinner dropdownSystem;
+    private Spinner dropdownGrade;
+    private BaseAdapter listAdapter = new BaseAdapter() {
+        @Override
+        public int getCount() {
+            return GradeSystem.printableValues().length;
+        }
+
+        @Override
+        public Object getItem(int i) {
+            return GradeSystem.printableValues()[i];
+        }
+
+        @Override
+        public long getItemId(int i) {
+            return i;
+        }
+
+        @Override
+        public View getView(int i, View view, ViewGroup viewGroup) {
+            if (view == null) {
+                view = inflater.inflate(R.layout.list_item_converter, viewGroup, false);
+            }
+            ((TextView)view.findViewById(R.id.unitValue)).setText(GradeSystem.printableValues()[i].getGrade(dropdownGrade.getSelectedItemPosition()));
+            ((TextView)view.findViewById(R.id.systemValue)).setText(parent.getString(GradeSystem.printableValues()[i].shortName));
+            return view;
+        }
+    };
+    private LayoutInflater inflater;
+
+    public LengthConverter(AppCompatActivity parent, @LayoutRes int viewID) {
+        super(parent, viewID);
+    }
+
+    @Override
+    public void onCreate(ViewGroup view) {
+        this.view = view;
+
+        inflater = parent.getLayoutInflater();
+
+        ((TextView)findViewById(R.id.gradingSelectLabel)).setText(parent.getResources().getString(R.string.grade_system,
+                parent.getResources().getString(GradeSystem.fromString(Globals.globalConfigs.getString(Configs.ConfigKey.converterGradeSystem)).shortName)));
+
+        dropdownSystem = findViewById(R.id.gradeSystemSpinner);
+        dropdownGrade = findViewById(R.id.gradeSelectSpinner);
+
+        dropdownSystem.setOnItemSelectedListener(null);
+        dropdownSystem.setAdapter(new GradeSystem.GradeSystemArrayAdapter(parent, android.R.layout.simple_spinner_dropdown_item, GradeSystem.printableValues()));
+
+        dropdownSystem.setSelection(GradeSystem.systemToPrintableIndex(GradeSystem.fromString(Globals.globalConfigs.getString(Configs.ConfigKey.converterGradeSystem))), false);
+        dropdownSystem.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+                Globals.globalConfigs.setString(Configs.ConfigKey.converterGradeSystem, GradeSystem.printableValues()[i].getMainKey());
+                ((TextView)findViewById(R.id.gradingSelectLabel)).setText(parent.getResources().getString(R.string.grade_system,
+                        parent.getResources().getString(GradeSystem.printableValues()[i].shortName)));
+                buildGradeDropdown(GradeSystem.printableValues()[i]);
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> adapterView) {
+
+            }
+        });
+
+        buildGradeDropdown(GradeSystem.printableValues()[dropdownSystem.getSelectedItemPosition()]);
+
+        ListView resultsList = findViewById(R.id.listGradesConverter);
+        resultsList.setAdapter(listAdapter);
+
+        findViewById(R.id.buttonShowGradesTable).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(parent, UnitConverterGradesAdvancedActivity.class);
+                parent.startActivity(intent);
+            }
+        });
+    }
+
+    private void buildGradeDropdown(GradeSystem system) {
+        Spinner dropdownGrade = findViewById(R.id.gradeSelectSpinner);
+        List<String> allGrades = system.getPureAllGrades();
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(parent, android.R.layout.simple_spinner_dropdown_item, allGrades) {
+            // Change color item
+            @Override
+            public View getDropDownView(int position, View convertView,
+                                        ViewGroup itemParent) {
+                View mView = super.getDropDownView(position, convertView, itemParent);
+                TextView mTextView = (TextView) mView;
+
+                mTextView.setBackgroundColor(Globals.gradeToColorState(position).getDefaultColor());
+                return mView;
+            }
+        };
+
+        dropdownGrade.setAdapter(adapter);
+        dropdownGrade.setSelection(Globals.globalConfigs.getInt(Configs.ConfigKey.converterGradeValue));
+        dropdownGrade.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+                Globals.globalConfigs.setInt(Configs.ConfigKey.converterGradeValue, i);
+                findViewById(R.id.gradeSelectSpinnerBackground).setBackgroundColor(Globals.gradeToColorState(i).getDefaultColor());
+                listAdapter.notifyDataSetChanged();
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> adapterView) {
+
+            }
+        });
+    }
+
+    @Override
+    public void onViewSelected() {
+
+    }
+}
