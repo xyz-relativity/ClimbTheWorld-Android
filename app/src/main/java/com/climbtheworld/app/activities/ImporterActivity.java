bdd0dd08d14dd326fd7a2dca3c56bb257814e2fb
xyz32 <xyz32@users.noreply.github.com>
2020-03-20 13:05:29 -0400

    Add a bit more description.

--- app/src/main/java/com/climbtheworld/app/activities/ImporterActivity.java
+++ app/src/main/java/com/climbtheworld/app/activities/ImporterActivity.java
@@ -213,13 +213,19 @@ public class ImporterActivity extends AppCompatActivity {
 
             case R.id.buttonImport:
                 AlertDialog.Builder builder = new AlertDialog.Builder(this);
-                builder.setTitle("Title");
+                builder.setTitle("Enter 'The Crag' area ID");
 
-                final ViewGroup group = new LinearLayout(this);
+                final LinearLayout group = new LinearLayout(this);
+                group.setOrientation(LinearLayout.VERTICAL);
+                final TextView info = new TextView(this);
+                info.setLayoutParams(new LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT, LinearLayout.LayoutParams.WRAP_CONTENT));
+                info.setText("The id is part of an area URL. It is the number after the word 'area': https://www.thecrag.com/climbing/united-states/red-river-gorge/area/14528689");
+                info.setPadding(0, 0, 0, 20);
+                group.addView(info);
+
                 final EditText input = new EditText(this);
-
                 input.setInputType(InputType.TYPE_CLASS_NUMBER);
-                input.setText("14528689");
+                input.setLayoutParams(new ViewGroup.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT));
                 group.addView(input);
 
                 builder.setView(group);
@@ -258,11 +264,12 @@ public class ImporterActivity extends AppCompatActivity {
 
                             @Override
                             protected void thenDoUiRelatedWork(Boolean result) {
-                                addedNodes.clear();
-                                nodesMap.clear();
-                                updateUI();
+//                                addedNodes.clear();
+//                                nodesMap.clear();
+//                                updateUI();
                             }
                         });
+                finish();
                 break;
         }
     }
@@ -271,10 +278,6 @@ public class ImporterActivity extends AppCompatActivity {
         Constants.WEB_EXECUTOR.execute(new Runnable() {
             public void run() {
                 try {
-                    if (IMPORT_COUNTER == 0) {
-                        buildClimbingNodes(testData);
-                        return;
-                    }
                     OkHttpClient httpClient = new OkHttpClient.Builder().connectTimeout(30, TimeUnit.SECONDS).readTimeout(30,
                             TimeUnit.SECONDS).cookieJar(new CookieJar() {
                         private final HashMap<String, List<Cookie>> cookieStore = new HashMap<>();
@@ -497,304 +500,6 @@ public class ImporterActivity extends AppCompatActivity {
             if (gradeIndex < tmpIndex) {
                 tags.put(minGradeKey, GradeSystem.uiaa.getGrade(gradeIndex));
             }
-        }
-    }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-    ///testdata
-    private static DownloadedData testData;
-
-    static {
-        try {
-            testData = new DownloadedData();
-            testData.theCrag = new JSONObject("{\n" +
-                    "      \"data\": {\n" +
-                    "        \"ascentCount\": \"43\",\n" +
-                    "        \"asciiName\": \"The Dark Side\",\n" +
-                    "        \"averageHeight\": [\n" +
-                    "          21,\n" +
-                    "          \"m\"\n" +
-                    "        ],\n" +
-                    "        \"childIDs\": [\n" +
-                    "          \"2310354318\",\n" +
-                    "          \"2310354417\",\n" +
-                    "          \"2310354519\",\n" +
-                    "          \"2310354633\",\n" +
-                    "          \"2310354768\",\n" +
-                    "          \"2310354900\",\n" +
-                    "          \"2310354999\",\n" +
-                    "          \"2310355098\",\n" +
-                    "          \"17098981\",\n" +
-                    "          \"1475763669\",\n" +
-                    "          \"2310361644\",\n" +
-                    "          \"17098927\",\n" +
-                    "          \"2310362544\",\n" +
-                    "          \"2310362643\",\n" +
-                    "          \"1475763774\",\n" +
-                    "          \"2310366030\",\n" +
-                    "          \"2310366174\",\n" +
-                    "          \"2310366273\",\n" +
-                    "          \"2310366372\",\n" +
-                    "          \"2310366471\",\n" +
-                    "          \"2310366570\",\n" +
-                    "          \"2310366669\",\n" +
-                    "          \"1957620387\",\n" +
-                    "          \"2310370254\",\n" +
-                    "          \"1495441935\",\n" +
-                    "          \"2310371646\",\n" +
-                    "          \"2724390285\",\n" +
-                    "          \"2724390537\",\n" +
-                    "          \"2724391023\",\n" +
-                    "          \"2724392262\",\n" +
-                    "          \"2724393753\",\n" +
-                    "          \"2724394488\",\n" +
-                    "          \"2724395004\",\n" +
-                    "          \"2724395238\",\n" +
-                    "          \"2724395517\",\n" +
-                    "          \"2724396516\",\n" +
-                    "          \"2724397752\"\n" +
-                    "        ],\n" +
-                    "        \"depth\": \"6\",\n" +
-                    "        \"displayAverageHeight\": [\n" +
-                    "          21,\n" +
-                    "          \"m\"\n" +
-                    "        ],\n" +
-                    "        \"hasUnarchivedChildren\": 1,\n" +
-                    "        \"hide\": {},\n" +
-                    "        \"id\": \"14529313\",\n" +
-                    "        \"kudos\": \"82\",\n" +
-                    "        \"lastPDFStaticDate\": \"2020-02-21T21:17:07Z\",\n" +
-                    "        \"lastPDFStaticSize\": \"524375\",\n" +
-                    "        \"locatedness\": 240,\n" +
-                    "        \"maxPop\": 30,\n" +
-                    "        \"name\": \"The Dark Side\",\n" +
-                    "        \"numberPhotos\": 1,\n" +
-                    "        \"numberRoutes\": \"37\",\n" +
-                    "        \"parentID\": \"195029640\",\n" +
-                    "        \"permitNode\": {\n" +
-                    "          \"node\": {\n" +
-                    "            \"id\": \"11900101\"\n" +
-                    "          }\n" +
-                    "        },\n" +
-                    "        \"priceCategory\": \"Medium\",\n" +
-                    "        \"redirectStubs\": [],\n" +
-                    "        \"seasonality\": [\n" +
-                    "          1,\n" +
-                    "          0,\n" +
-                    "          3,\n" +
-                    "          2,\n" +
-                    "          3,\n" +
-                    "          1,\n" +
-                    "          4,\n" +
-                    "          2,\n" +
-                    "          1,\n" +
-                    "          18,\n" +
-                    "          1,\n" +
-                    "          6\n" +
-                    "        ],\n" +
-                    "        \"subType\": \"Cliff\",\n" +
-                    "        \"tlc\": {\n" +
-                    "          \"node\": {\n" +
-                    "            \"id\": \"11900101\"\n" +
-                    "          }\n" +
-                    "        },\n" +
-                    "        \"type\": \"area\",\n" +
-                    "        \"urlAncestorStub\": \"united-states\\/red-river-gorge\"\n" +
-                    "      },\n" +
-                    "      \"query\": {\n" +
-                    "        \"entity\": \"node\",\n" +
-                    "        \"id\": \"14529313\",\n" +
-                    "        \"request\": \"id\"\n" +
-                    "      }\n" +
-                    "    }");
-
-            testData.theRoutes.add(new JSONObject("{\n" +
-                    "      \"data\": {\n" +
-                    "        \"asciiName\": \"Dagobah\",\n" +
-                    "        \"context\": \"US\",\n" +
-                    "        \"cragScore\": 1.99,\n" +
-                    "        \"depth\": \"7\",\n" +
-                    "        \"displayHeight\": [\n" +
-                    "          24,\n" +
-                    "          \"m\"\n" +
-                    "        ],\n" +
-                    "        \"flags\": {\n" +
-                    "          \"IsFree\": 1,\n" +
-                    "          \"IsSport\": 1\n" +
-                    "        },\n" +
-                    "        \"grade\": \"5.12a\",\n" +
-                    "        \"gradeAtom\": {\n" +
-                    "          \"context\": \"US\",\n" +
-                    "          \"grade\": \"5.12a\",\n" +
-                    "          \"gradeBand\": 3,\n" +
-                    "          \"gradeInContext\": \"5.12a\",\n" +
-                    "          \"gradeStyle\": \"Free\",\n" +
-                    "          \"internal\": [\n" +
-                    "            289,\n" +
-                    "            300\n" +
-                    "          ],\n" +
-                    "          \"type\": \"grade\"\n" +
-                    "        },\n" +
-                    "        \"gradeBand\": 3,\n" +
-                    "        \"gradeInContext\": \"5.12a\",\n" +
-                    "        \"gradeStyle\": \"Free\",\n" +
-                    "        \"height\": [\n" +
-                    "          \"24.38\",\n" +
-                    "          \"m\"\n" +
-                    "        ],\n" +
-                    "        \"hide\": {},\n" +
-                    "        \"id\": \"2310354318\",\n" +
-                    "        \"name\": \"Dagobah\",\n" +
-                    "        \"parentID\": \"14529313\",\n" +
-                    "        \"pitches\": \"1\",\n" +
-                    "        \"priceCategory\": \"Medium\",\n" +
-                    "        \"rawGrade\": [\n" +
-                    "          289,\n" +
-                    "          300\n" +
-                    "        ],\n" +
-                    "        \"stars\": \"0\",\n" +
-                    "        \"style\": \"Sport\",\n" +
-                    "        \"tlc\": {\n" +
-                    "          \"node\": {\n" +
-                    "            \"id\": \"11900101\"\n" +
-                    "          }\n" +
-                    "        },\n" +
-                    "        \"type\": \"route\",\n" +
-                    "        \"urlAncestorStub\": \"united-states\\/red-river-gorge\"\n" +
-                    "      },\n" +
-                    "      \"query\": {\n" +
-                    "        \"entity\": \"node\",\n" +
-                    "        \"id\": \"2310354318\",\n" +
-                    "        \"request\": \"id\"\n" +
-                    "      }\n" +
-                    "    }"));
-
-            testData.theRoutes.add(new JSONObject("{\n" +
-                    "      \"data\": {\n" +
-                    "        \"asciiName\": \"Overdrive\",\n" +
-                    "        \"canDelete\": 1,\n" +
-                    "        \"context\": \"US\",\n" +
-                    "        \"cragScore\": 1,\n" +
-                    "        \"depth\": \"7\",\n" +
-                    "        \"displayHeight\": [\n" +
-                    "          21,\n" +
-                    "          \"m\"\n" +
-                    "        ],\n" +
-                    "        \"flags\": {\n" +
-                    "          \"IsFree\": 1,\n" +
-                    "          \"IsTrad\": 1\n" +
-                    "        },\n" +
-                    "        \"grade\": \"5.9\",\n" +
-                    "        \"gradeAtom\": {\n" +
-                    "          \"context\": \"US\",\n" +
-                    "          \"grade\": \"5.9\",\n" +
-                    "          \"gradeBand\": 2,\n" +
-                    "          \"gradeInContext\": \"5.9\",\n" +
-                    "          \"gradeStyle\": \"Free\",\n" +
-                    "          \"internal\": [\n" +
-                    "            161,\n" +
-                    "            180\n" +
-                    "          ],\n" +
-                    "          \"type\": \"grade\"\n" +
-                    "        },\n" +
-                    "        \"gradeBand\": 2,\n" +
-                    "        \"gradeInContext\": \"5.9\",\n" +
-                    "        \"gradeStyle\": \"Free\",\n" +
-                    "        \"height\": [\n" +
-                    "          \"21.34\",\n" +
-                    "          \"m\"\n" +
-                    "        ],\n" +
-                    "        \"hide\": {},\n" +
-                    "        \"id\": \"220518252\",\n" +
-                    "        \"name\": \"Overdrive\",\n" +
-                    "        \"parentID\": \"14529289\",\n" +
-                    "        \"pitches\": \"1\",\n" +
-                    "        \"priceCategory\": \"Medium\",\n" +
-                    "        \"rawGrade\": [\n" +
-                    "          161,\n" +
-                    "          180\n" +
-                    "        ],\n" +
-                    "        \"stars\": \"0\",\n" +
-                    "        \"style\": \"Trad\",\n" +
-                    "        \"tlc\": {\n" +
-                    "          \"node\": {\n" +
-                    "            \"id\": \"11900101\"\n" +
-                    "          }\n" +
-                    "        },\n" +
-                    "        \"type\": \"route\",\n" +
-                    "        \"urlAncestorStub\": \"united-states\\/red-river-gorge\"\n" +
-                    "      },\n" +
-                    "      \"query\": {\n" +
-                    "        \"entity\": \"node\",\n" +
-                    "        \"id\": \"220518252\",\n" +
-                    "        \"request\": \"id\"\n" +
-                    "      }\n" +
-                    "    }"));
-
-            testData.theRoutes.add(new JSONObject("\n" +
-                    "\n" +
-                    "{\n" +
-                    "    \"data\": {\n" +
-                    "        \"asciiName\": \"LOMM\",\n" +
-                    "        \"bolts\": \"8\",\n" +
-                    "        \"canDelete\": 1,\n" +
-                    "        \"depth\": \"7\",\n" +
-                    "        \"displayHeight\": [\n" +
-                    "            24,\n" +
-                    "            \"m\"\n" +
-                    "        ],\n" +
-                    "        \"flags\": {\n" +
-                    "            \"IsProject\": 1,\n" +
-                    "            \"IsSport\": 1\n" +
-                    "        },\n" +
-                    "        \"gradeAtom\": {\n" +
-                    "            \"gradeBand\": 0,\n" +
-                    "            \"gradeStyle\": \"Unknown\",\n" +
-                    "            \"type\": \"grade\"\n" +
-                    "        },\n" +
-                    "        \"gradeBand\": 0,\n" +
-                    "        \"gradeStyle\": \"Unknown\",\n" +
-                    "        \"height\": [\n" +
-                    "            \"24.38\",\n" +
-                    "            \"m\"\n" +
-                    "        ],\n" +
-                    "        \"hide\": [],\n" +
-                    "        \"id\": \"747679047\",\n" +
-                    "        \"name\": \"LOMM\",\n" +
-                    "        \"parentID\": \"501158076\",\n" +
-                    "        \"pitches\": \"1\",\n" +
-                    "        \"priceCategory\": \"Medium\",\n" +
-                    "        \"stars\": \"0\",\n" +
-                    "        \"style\": \"Sport\",\n" +
-                    "        \"tlc\": {\n" +
-                    "            \"node\": {\n" +
-                    "                \"id\": \"11900101\"\n" +
-                    "            }\n" +
-                    "        },\n" +
-                    "        \"type\": \"route\",\n" +
-                    "        \"urlAncestorStub\": \"united-states\\/red-river-gorge\"\n" +
-                    "    },\n" +
-                    "    \"query\": {\n" +
-                    "        \"entity\": \"node\",\n" +
-                    "        \"id\": \"747679047\",\n" +
-                    "        \"request\": \"id\"\n" +
-                    "    }\n" +
-                    "}\n" +
-                    "\n"));
-        } catch (JSONException e) {
-            e.printStackTrace();
         }
     }
 }
