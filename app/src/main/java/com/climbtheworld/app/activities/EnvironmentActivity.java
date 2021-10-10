5d892bdfbdb873490c12200453c10137988f547d
xyz32 <xyz.relativity@gmail.com>
2021-10-10 10:37:01 -0400

    Add support for sunrise and sunset times.

--- app/src/main/java/com/climbtheworld/app/activities/EnvironmentActivity.java
+++ app/src/main/java/com/climbtheworld/app/activities/EnvironmentActivity.java
@@ -20,18 +20,19 @@ import com.climbtheworld.app.navigate.widgets.CompassWidget;
 import com.climbtheworld.app.sensors.environment.EnvironmentalSensors;
 import com.climbtheworld.app.sensors.environment.IEnvironmentListener;
 import com.climbtheworld.app.sensors.location.DeviceLocationManager;
-import com.climbtheworld.app.sensors.location.ILocationListener;
-import com.climbtheworld.app.sensors.orientation.IOrientationListener;
 import com.climbtheworld.app.sensors.orientation.OrientationManager;
 import com.climbtheworld.app.utils.Globals;
 import com.climbtheworld.app.utils.Quaternion;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
-import com.google.android.material.navigation.NavigationBarView;
 
+import org.shredzone.commons.suncalc.SunTimes;
+
+import java.text.DateFormat;
 import java.text.DecimalFormat;
+import java.util.Date;
 import java.util.Locale;
 
-public class EnvironmentActivity extends AppCompatActivity implements ILocationListener, IEnvironmentListener {
+public class EnvironmentActivity extends AppCompatActivity implements IEnvironmentListener {
 
 	private DeviceLocationManager deviceLocationManager;
 	private OrientationManager orientationManager;
@@ -40,9 +41,13 @@ public class EnvironmentActivity extends AppCompatActivity implements ILocationL
 	private static final int LOCATION_UPDATE_DELAY_MS = 500;
 	private static final String COORD_VALUE = "%.6f";
 	DecimalFormat decimalFormat = new DecimalFormat("000.0°");
+	DateFormat format;
+
 	private TextView editLatitude;
 	private TextView editLongitude;
 	private TextView editElevation;
+	private TextView editSunrise;
+	private TextView editSunset;
 	private TextView editAzimuthName;
 	private TextView editAzimuthValue;
 	private BottomNavigationView navigation;
@@ -54,6 +59,7 @@ public class EnvironmentActivity extends AppCompatActivity implements ILocationL
 	private TextView editRelativeHumidity;
 	private TextView editDewPoint;
 	private TextView editAbsoluteHumidity;
+	private CompassWidget compass;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -69,58 +75,13 @@ public class EnvironmentActivity extends AppCompatActivity implements ILocationL
 		viewSwitcher = findViewById(R.id.viewEnvSwitcher);
 		viewSwitcher.findViewById(R.id.mapViewContainer).setVisibility(View.GONE);
 
-		navigation = findViewById(R.id.buttonsNavigationBar);
-		navigation.setItemIconTintList(null);
-
-		navigation.setOnItemSelectedListener(new NavigationBarView.OnItemSelectedListener() {
-			@Override
-			public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
-				switch (menuItem.getItemId()) {
-					case R.id.map_navigation:
-						viewSwitcher.findViewById(R.id.mapViewContainer).setVisibility(View.VISIBLE);
-						viewSwitcher.findViewById(R.id.sensorViewContainer).setVisibility(View.GONE);
-						return true;
-					case R.id.sensor_navigation:
-						viewSwitcher.findViewById(R.id.mapViewContainer).setVisibility(View.GONE);
-						viewSwitcher.findViewById(R.id.sensorViewContainer).setVisibility(View.VISIBLE);
-						return true;
-				}
-				return false;
-			}
-		});
-
-		mapWidget = MapWidgetBuilder.getBuilder(this, true).build();
-
-		final CompassWidget compass = new CompassWidget(findViewById(R.id.compassFace));
-
-		//location
-		deviceLocationManager = new DeviceLocationManager(this, LOCATION_UPDATE_DELAY_MS, this);
-
-		orientationManager = new OrientationManager(this, SensorManager.SENSOR_DELAY_UI);
-		orientationManager.addListener(new IOrientationListener() {
-			@Override
-			public void updateOrientation(OrientationManager.OrientationEvent event) {
-				Globals.virtualCamera.updateOrientation(event);
-
-				mapWidget.onOrientationChange(event.screen);
-
-				editAzimuthName.setText(AugmentedRealityUtils.getStringBearings(EnvironmentActivity.this, event.screen.x));
-
-				editLatitude.setText(String.format(Locale.getDefault(), COORD_VALUE, Globals.virtualCamera.decimalLatitude));
-				editLongitude.setText(String.format(Locale.getDefault(), COORD_VALUE, Globals.virtualCamera.decimalLongitude));
-				editElevation.setText(String.format(Locale.getDefault(), COORD_VALUE, Globals.virtualCamera.elevationMeters));
-
-				editAzimuthValue.setText(decimalFormat.format(event.screen.x));
-				compass.updateOrientation(event.screen);
-			}
-		});
-
-		sensorManager = new EnvironmentalSensors(this);
-		sensorManager.addListener(this);
-
 		editLatitude = findViewById(R.id.editLatitude);
 		editLongitude = findViewById(R.id.editLongitude);
 		editElevation = findViewById(R.id.editElevation);
+
+		editSunrise = findViewById(R.id.editSunrise);
+		editSunset = findViewById(R.id.editSunset);
+
 		editAzimuthName = findViewById(R.id.editAzimuthName);
 		editAzimuthValue = findViewById(R.id.editAzimuthValue);
 
@@ -130,11 +91,42 @@ public class EnvironmentActivity extends AppCompatActivity implements ILocationL
 		editRelativeHumidity = findViewById(R.id.editRelativeHumidity);
 		editDewPoint = findViewById(R.id.editDewPoint);
 		editAbsoluteHumidity = findViewById(R.id.editAbsoluteHunidity);
+
+		format = android.text.format.DateFormat.getTimeFormat(this);
+
+		navigation = findViewById(R.id.buttonsNavigationBar);
+		navigation.setItemIconTintList(null);
+
+		navigation.setOnItemSelectedListener(this::onNavigationItemSelected);
+
+		mapWidget = MapWidgetBuilder.getBuilder(this, true).build();
+
+		compass = new CompassWidget(findViewById(R.id.compassFace));
+
+		//location
+		deviceLocationManager = new DeviceLocationManager(this, LOCATION_UPDATE_DELAY_MS, this::updatePosition);
+
+		orientationManager = new OrientationManager(this, SensorManager.SENSOR_DELAY_UI);
+		orientationManager.addListener(this::updateOrientation);
+
+		sensorManager = new EnvironmentalSensors(this);
+		sensorManager.addListener(this);
 	}
 
-	@Override
 	public void updatePosition(double pDecLatitude, double pDecLongitude, double pMetersAltitude, double accuracy) {
 		Globals.virtualCamera.updatePOILocation(pDecLatitude, pDecLongitude, pMetersAltitude);
+
+		editLatitude.setText(String.format(Locale.getDefault(), COORD_VALUE, Globals.virtualCamera.decimalLatitude));
+		editLongitude.setText(String.format(Locale.getDefault(), COORD_VALUE, Globals.virtualCamera.decimalLongitude));
+		editElevation.setText(String.format(Locale.getDefault(), COORD_VALUE, Globals.virtualCamera.elevationMeters));
+
+		SunTimes times = SunTimes.compute()
+				.on(new Date())   // set a date
+				.at(pDecLatitude, pDecLongitude)   // set a location
+				.execute();     // get the results
+
+		editSunrise.setText(format.format(times.getRise()));
+		editSunset.setText(format.format(times.getSet()));
 
 		mapWidget.onLocationChange(Globals.geoNodeToGeoPoint(Globals.virtualCamera));
 	}
@@ -161,6 +153,17 @@ public class EnvironmentActivity extends AppCompatActivity implements ILocationL
 		super.onPause();
 	}
 
+	public void updateOrientation(OrientationManager.OrientationEvent event) {
+		Globals.virtualCamera.updateOrientation(event);
+
+		mapWidget.onOrientationChange(event.screen);
+
+		editAzimuthName.setText(AugmentedRealityUtils.getStringBearings(EnvironmentActivity.this, event.screen.x));
+
+		editAzimuthValue.setText(decimalFormat.format(event.screen.x));
+		compass.updateOrientation(event.screen);
+	}
+
 	@Override
 	public void updateSensors(Quaternion sensors) {
 		editTemperature.setText(String.format(Locale.getDefault(), "%.2f (°C)",sensors.w));
@@ -177,5 +180,19 @@ public class EnvironmentActivity extends AppCompatActivity implements ILocationL
 
 		double absoluteHumidity = 216.7*(((sensors.z/100)*6.112*Math.exp(tempFactor))/(273.15 + sensors.w));
 		editAbsoluteHumidity.setText(String.format(Locale.getDefault(), "%.2f (g/㎥)",absoluteHumidity));
+	}
+
+	public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+		switch (menuItem.getItemId()) {
+			case R.id.map_navigation:
+				viewSwitcher.findViewById(R.id.mapViewContainer).setVisibility(View.VISIBLE);
+				viewSwitcher.findViewById(R.id.sensorViewContainer).setVisibility(View.GONE);
+				return true;
+			case R.id.sensor_navigation:
+				viewSwitcher.findViewById(R.id.mapViewContainer).setVisibility(View.GONE);
+				viewSwitcher.findViewById(R.id.sensorViewContainer).setVisibility(View.VISIBLE);
+				return true;
+		}
+		return false;
 	}
 }
