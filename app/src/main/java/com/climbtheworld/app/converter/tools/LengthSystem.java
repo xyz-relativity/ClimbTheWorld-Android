7112a0d718efad173736b266bd9587003d528d90
xyz32 <xyz.relativity@gmail.com>
2022-01-11 15:55:59 -0500

    Refactor converters a bit.

--- app/src/main/java/com/climbtheworld/app/converter/tools/LengthSystem.java
+++ app/src/main/java/com/climbtheworld/app/converter/tools/LengthSystem.java
@@ -4,28 +4,106 @@ import com.climbtheworld.app.R;
 import com.climbtheworld.app.configs.Configs;
 
 public enum LengthSystem {
-	meter(R.string.unit_system_meter, R.string.unit_system_meter_short, R.string.unit_system_meter_description, 1.0, 1.0),
-	mile(R.string.unit_system_mile, R.string.unit_system_mile_short, R.string.unit_system_mile_description, 1609.344, 0.00062137119223733397),
-	yard(R.string.unit_system_yard, R.string.unit_system_yard_short, R.string.unit_system_yard_description, 0.9144, 1.09361329833770779),
-	feet(R.string.unit_system_feet, R.string.unit_system_feet_short, R.string.unit_system_feet_description, 0.3048, 3.28083989501312336),
-	inch(R.string.unit_system_inch, R.string.unit_system_inch_short, R.string.unit_system_inch_description, 0.0254, 39.3700787401574803),
-	nauticalMile(R.string.unit_system_nautical_mile, R.string.unit_system_nautical_mile_short, R.string.unit_system_meter, 1852.0, 0.000539956803455723542),
-	furlong(R.string.unit_system_furlong, R.string.unit_system_furlong_short, R.string.unit_system_furlong_description, 201.168, 0.0049709695379),
-	lightYear(R.string.unit_system_lightyear, R.string.unit_system_lightyear_short, R.string.unit_system_lightyear_description, 9460730472580800.0, 0.0000000000000001057000834024615463709);
+	meter(R.string.unit_system_meter, R.string.unit_system_meter_short, R.string.unit_system_meter_description, new UnitConverter() {
+		@Override
+		public double convertToSI(double value) {
+			return value;
+		}
 
-	LengthSystem(int localeName, int shortName, int description, double conversionToBase, double conversionFromBase) {
+		@Override
+		public double convertFromSI(double value) {
+			return value;
+		}
+	}),
+	mile(R.string.unit_system_mile, R.string.unit_system_mile_short, R.string.unit_system_mile_description, new UnitConverter() {
+		@Override
+		public double convertToSI(double value) {
+			return value * 1609.344;
+		}
+
+		@Override
+		public double convertFromSI(double value) {
+			return value * 0.00062137119223733397;
+		}
+	}),
+	yard(R.string.unit_system_yard, R.string.unit_system_yard_short, R.string.unit_system_yard_description, new UnitConverter() {
+		@Override
+		public double convertToSI(double value) {
+			return value * 0.9144;
+		}
+
+		@Override
+		public double convertFromSI(double value) {
+			return value * 1.09361329833770779;
+		}
+	}),
+	feet(R.string.unit_system_feet, R.string.unit_system_feet_short, R.string.unit_system_feet_description, new UnitConverter() {
+		@Override
+		public double convertToSI(double value) {
+			return value * 0.3048;
+		}
+
+		@Override
+		public double convertFromSI(double value) {
+			return value * 3.28083989501312336;
+		}
+	}),
+	inch(R.string.unit_system_inch, R.string.unit_system_inch_short, R.string.unit_system_inch_description, new UnitConverter() {
+		@Override
+		public double convertToSI(double value) {
+			return value * 0.0254;
+		}
+
+		@Override
+		public double convertFromSI(double value) {
+			return value * 39.3700787401574803;
+		}
+	}),
+	nauticalMile(R.string.unit_system_nautical_mile, R.string.unit_system_nautical_mile_short, R.string.unit_system_meter, new UnitConverter() {
+		@Override
+		public double convertToSI(double value) {
+			return value * 1852.0;
+		}
+
+		@Override
+		public double convertFromSI(double value) {
+			return value * 0.000539956803455723542;
+		}
+	}),
+	furlong(R.string.unit_system_furlong, R.string.unit_system_furlong_short, R.string.unit_system_furlong_description, new UnitConverter() {
+		@Override
+		public double convertToSI(double value) {
+			return value * 201.168;
+		}
+
+		@Override
+		public double convertFromSI(double value) {
+			return value * 0.0049709695379;
+		}
+	}),
+	lightYear(R.string.unit_system_lightyear, R.string.unit_system_lightyear_short, R.string.unit_system_lightyear_description, new UnitConverter() {
+		@Override
+		public double convertToSI(double value) {
+			return value * 9460730472580800.0;
+		}
+
+		@Override
+		public double convertFromSI(double value) {
+			return value * 0.0000000000000001057000834024615463709;
+		}
+	});
+
+	LengthSystem(int localeName, int shortName, int description, UnitConverter converter) {
 		this.localeName = localeName;
 		this.shortName = shortName;
 		this.description = description;
-		this.conversionFromBase = conversionFromBase;
-		this.conversionToBase = conversionToBase;
+		this.converter = converter;
 	}
 
 	private final int localeName;
 	private final int shortName;
 	private final int description;
-	private final double conversionToBase;
-	private final double conversionFromBase;
+	private final UnitConverter converter;
 
 	public static LengthSystem fromString(String value) {
 		LengthSystem result = LengthSystem.valueOf(Configs.ConfigKey.converterLengthSystem.defaultVal.toString());
@@ -50,19 +128,11 @@ public enum LengthSystem {
 		return description;
 	}
 
-	public double getConversionToBase() {
-		return conversionToBase;
-	}
-
-	public double getConversionFromBase() {
-		return conversionFromBase;
+	public UnitConverter getConverter() {
+		return converter;
 	}
 
 	public double convertTo(LengthSystem toSystem, double value) {
-		return value * conversionToBase * toSystem.getConversionFromBase();
-	}
-
-	public double convertFrom(LengthSystem fromSystem, double value) {
-		return value * fromSystem.getConversionToBase() * conversionFromBase;
+		return toSystem.getConverter().convertFromSI(getConverter().convertToSI(value));
 	}
 }
--- app/src/main/java/com/climbtheworld/app/converter/tools/WeightSystem.java
+++ app/src/main/java/com/climbtheworld/app/converter/tools/WeightSystem.java
@@ -4,27 +4,95 @@ import com.climbtheworld.app.R;
 import com.climbtheworld.app.configs.Configs;
 
 public enum WeightSystem {
-	kiloGram(R.string.unit_system_kilogram, R.string.unit_system_kilogram_short, R.string.unit_system_kilogram_description, 1.0, 1.0),
-	pound(R.string.unit_system_pound, R.string.unit_system_pound_short, R.string.unit_system_pound_description, 0.45359237, 2.20462262184877581),
-	ounce(R.string.unit_system_ounce, R.string.unit_system_ounce_short, R.string.unit_system_ounce_description, 0.028349523125, 35.27396194958041291568),
-	grain(R.string.unit_system_grain, R.string.unit_system_grain_short, R.string.unit_system_grain_description, 0.00006479891, 15432.35835294143065061),
-	stone(R.string.unit_system_stone, R.string.unit_system_stone_short, R.string.unit_system_stone_description, 6.35029318, 0.15747304441777),
-	shortTon(R.string.unit_system_short_tone, R.string.unit_system_short_tone_short, R.string.unit_system_short_tone_description, 907.18474, 0.0011023113109243879),
-	longTon(R.string.unit_system_long_tone, R.string.unit_system_long_tone_short, R.string.unit_system_long_tone_description, 1016.0469088, 0.0009842065276110606282276);
+	kiloGram(R.string.unit_system_kilogram, R.string.unit_system_kilogram_short, R.string.unit_system_kilogram_description, new UnitConverter() {
+		@Override
+		public double convertToSI(double value) {
+			return value;
+		}
 
-	WeightSystem(int localeName, int shortName, int description, double conversionToBase, double conversionFromBase) {
+		@Override
+		public double convertFromSI(double value) {
+			return value;
+		}
+	}),
+	pound(R.string.unit_system_pound, R.string.unit_system_pound_short, R.string.unit_system_pound_description, new UnitConverter() {
+		@Override
+		public double convertToSI(double value) {
+			return value * 0.45359237;
+		}
+
+		@Override
+		public double convertFromSI(double value) {
+			return value * 2.20462262184877581;
+		}
+	}),
+	ounce(R.string.unit_system_ounce, R.string.unit_system_ounce_short, R.string.unit_system_ounce_description, new UnitConverter() {
+		@Override
+		public double convertToSI(double value) {
+			return value * 0.028349523125;
+		}
+
+		@Override
+		public double convertFromSI(double value) {
+			return value * 35.27396194958041291568;
+		}
+	}),
+	grain(R.string.unit_system_grain, R.string.unit_system_grain_short, R.string.unit_system_grain_description, new UnitConverter() {
+		@Override
+		public double convertToSI(double value) {
+			return value * 0.00006479891;
+		}
+
+		@Override
+		public double convertFromSI(double value) {
+			return value * 15432.35835294143065061;
+		}
+	}),
+	stone(R.string.unit_system_stone, R.string.unit_system_stone_short, R.string.unit_system_stone_description, new UnitConverter() {
+		@Override
+		public double convertToSI(double value) {
+			return value * 6.35029318;
+		}
+
+		@Override
+		public double convertFromSI(double value) {
+			return value * 0.15747304441777;
+		}
+	}),
+	shortTon(R.string.unit_system_short_tone, R.string.unit_system_short_tone_short, R.string.unit_system_short_tone_description, new UnitConverter() {
+		@Override
+		public double convertToSI(double value) {
+			return value * 907.18474;
+		}
+
+		@Override
+		public double convertFromSI(double value) {
+			return value * 0.0011023113109243879;
+		}
+	}),
+	longTon(R.string.unit_system_long_tone, R.string.unit_system_long_tone_short, R.string.unit_system_long_tone_description, new UnitConverter() {
+		@Override
+		public double convertToSI(double value) {
+			return value * 1016.0469088;
+		}
+
+		@Override
+		public double convertFromSI(double value) {
+			return value * 0.0009842065276110606282276;
+		}
+	});
+
+	WeightSystem(int localeName, int shortName, int description, UnitConverter converter) {
 		this.localeName = localeName;
 		this.shortName = shortName;
 		this.description = description;
-		this.conversionFromBase = conversionFromBase;
-		this.conversionToBase = conversionToBase;
+		this.converter = converter;
 	}
 
 	private final int localeName;
 	private final int shortName;
 	private final int description;
-	private final double conversionToBase;
-	private final double conversionFromBase;
+	private final UnitConverter converter;
 
 	public static WeightSystem fromString(String value) {
 		WeightSystem result = WeightSystem.valueOf(Configs.ConfigKey.converterWeightSystem.defaultVal.toString());
@@ -49,19 +117,11 @@ public enum WeightSystem {
 		return description;
 	}
 
-	public double getConversionToBase() {
-		return conversionToBase;
-	}
-
-	public double getConversionFromBase() {
-		return conversionFromBase;
+	public UnitConverter getConverter() {
+		return converter;
 	}
 
 	public double convertTo(WeightSystem toSystem, double value) {
-		return value * conversionToBase * toSystem.getConversionFromBase();
-	}
-
-	public double convertFrom(WeightSystem fromSystem, double value) {
-		return value * fromSystem.getConversionToBase() * conversionFromBase;
+		return toSystem.getConverter().convertFromSI(getConverter().convertToSI(value));
 	}
 }
