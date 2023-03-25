94e2a86e3ff9ccff2d57d60ecb73972d5f1b722c
xyz32 <xyz.relativity@gmail.com>
2023-03-25 11:37:07 -0400

    Implement areas as nodes support.

--- app/src/main/java/com/climbtheworld/app/map/widget/ClimbingAreaOverlayWidget.java
+++ app/src/main/java/com/climbtheworld/app/map/widget/ClimbingAreaOverlayWidget.java
@@ -1,5 +1,11 @@
 package com.climbtheworld.app.map.widget;
 
+import android.view.View;
+import android.widget.TextView;
+
+import androidx.appcompat.content.res.AppCompatResources;
+
+import com.climbtheworld.app.R;
 import com.climbtheworld.app.storage.DataManagerNew;
 import com.climbtheworld.app.storage.database.ClimbingTags;
 import com.climbtheworld.app.storage.database.OsmCollectionEntity;
@@ -14,6 +20,7 @@ import org.osmdroid.views.MapView;
 import org.osmdroid.views.overlay.FolderOverlay;
 import org.osmdroid.views.overlay.Marker;
 import org.osmdroid.views.overlay.Polygon;
+import org.osmdroid.views.overlay.infowindow.InfoWindow;
 
 import java.util.HashMap;
 import java.util.Iterator;
@@ -41,15 +48,19 @@ public class ClimbingAreaOverlayWidget extends ClimbingOverlayWidget {
 	public void refresh(BoundingBox bBox, boolean cancelable, UiRelatedTask<Boolean> booleanUiRelatedTask) {
 		List<Long> osmRelations = downloadManagerNew.loadCollectionBBox(osmMap.getContext(), bBox, OsmEntity.EntityClimbingType.area);
 		List<Long> osmNodes = downloadManagerNew.loadNodeBBox(osmMap.getContext(), bBox, OsmEntity.EntityClimbingType.area);
-		viewCleanup(bBox, osmRelations, osmNodes);
+		cleanupRelations(bBox, osmRelations);
 		renderRelations(osmRelations);
+
+		cleanupNodes(bBox, osmNodes);
+		renderNodes(osmNodes);
 	}
 
-	private void viewCleanup(BoundingBox bBox, List<Long> osmRelations, List<Long> osmNodes) {
+	private void cleanupRelations(BoundingBox bBox, List<Long> osmRelations) {
 		for(Iterator<Map.Entry<Long, PolygonWithCenter>> it = visibleAreaCache.entrySet().iterator(); it.hasNext(); ) {
 			Map.Entry<Long, PolygonWithCenter> entry = it.next();
 			if(!bBox.overlaps(entry.getValue().polygon.getBounds(), 99)) {
 				climbingAreaOverlayFolder.remove(entry.getValue().polygon);
+				entry.getValue().center.closeInfoWindow();
 				climbingPointOverlayFolder.remove(entry.getValue().center);
 				visibleMarkerCache.remove(entry.getKey());
 				it.remove();
@@ -102,6 +113,62 @@ public class ClimbingAreaOverlayWidget extends ClimbingOverlayWidget {
 
 			climbingPointOverlayFolder.add(center);
 			visibleMarkerCache.put(collection.osmID, center);
+		}
+	}
+
+	private void cleanupNodes(BoundingBox bBox, List<Long> osmBBox) {
+		for(Iterator<Map.Entry<Long, Marker>> it = visibleMarkerCache.entrySet().iterator(); it.hasNext(); ) {
+			Map.Entry<Long, Marker> entry = it.next();
+			if (osmMap.getZoomLevelDouble() > ZOOM_THRESHOLD) {
+				entry.getValue().closeInfoWindow();
+				climbingPointOverlayFolder.remove(entry.getValue());
+				it.remove();
+			} else if(!bBox.contains(entry.getValue().getPosition())) {
+				entry.getValue().closeInfoWindow();
+				climbingPointOverlayFolder.remove(entry.getValue());
+				it.remove();
+			}
+		}
+
+		for(Iterator<Long> it = osmBBox.iterator(); it.hasNext(); ) {
+			Long entry = it.next();
+			if(visibleMarkerCache.containsKey(entry)) {
+				it.remove();
+			}
+		}
+	}
+
+	private void renderNodes(List<Long> osmBBox) {
+		Map<Long, OsmNode> nodes = downloadManagerNew.loadNodeData(osmMap.getContext(), osmBBox);
+		for (OsmNode node: nodes.values()) {
+			Marker center = new Marker(osmMap);
+			center.setPosition(node.toGeoPoint());
+			center.setId(String.valueOf(node.osmID));
+			center.setTitle(node.getTags().optString(ClimbingTags.KEY_NAME));
+			center.setIcon(AppCompatResources.getDrawable(osmMap.getContext(), R.drawable.ic_poi_info));
+			center.setInfoWindow(new InfoWindow(R.layout.fragment_info_window_route, osmMap) {
+				@Override
+				public void onOpen(Object item) {
+					closeAllInfoWindowsOn(osmMap);
+					mView.setAlpha((float) 0.94);
+					((TextView)mView.findViewById(R.id.textTitle)).setText(center.getTitle());
+					mView.setOnClickListener(new View.OnClickListener() {
+						@Override
+						public void onClick(View v) {
+							close();
+						}
+					});
+				}
+
+				@Override
+				public void onClose() {
+
+				}
+			});
+			center.setAnchor(Marker.ANCHOR_CENTER,Marker.ANCHOR_BOTTOM);
+			visibleMarkerCache.put(node.osmID, center);
+
+			climbingPointOverlayFolder.add(center);
 		}
 	}
 }
