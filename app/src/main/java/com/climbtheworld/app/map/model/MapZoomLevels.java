13f8ecb48b1c10858fbfb3c80f6b75918d3812fa
Radu <xyz.relativiry@gmail.com>
2026-09-24 19:38:36 -0400

    Tune zoom levels.

--- app/src/main/java/com/climbtheworld/app/map/model/MapZoomLevels.java
+++ app/src/main/java/com/climbtheworld/app/map/model/MapZoomLevels.java
@@ -4,9 +4,9 @@ package com.climbtheworld.app.map.model;
  * Shared zoom thresholds for the map's semantic detail levels.
  */
 public final class MapZoomLevels {
-	public static final float AREA_MIN = 9f;
-	public static final float CRAG_MIN = 11f;
-	public static final float POI_AND_ROUTE_MIN = 16f;
+	public static final float AREA_MIN = 7f;
+	public static final float CRAG_MIN = 14f;
+	public static final float POI_AND_ROUTE_MIN = 19f;
 
 	private MapZoomLevels() {
 	}
